4c337e2b6e1a2c8fe278bb0338481f1bbf6330fa
Daniel-Andarge <andargedaniel90@gmail.com>
2024-06-26 21:00:51 +0300

    Add dockerfile

--- flask-app/serve_model.py
+++ flask-app/serve_model.py
@@ -4,13 +4,13 @@ import pickle
 import os
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__)))
 from scripts.feature_engineering import FeatureEngineering
 
 app = Flask(__name__)
 
 # Load model
-with open('../models/fraud_detection_xgb_model.pkl', 'rb') as f:
+with open('models/fraud_detection_xgb_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/')
